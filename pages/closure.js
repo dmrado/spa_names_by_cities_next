798302e64d73a86810f3ccc14a01b67212afdef7
CreatiVe <naukomania@gmail.com>
2019-07-24 01:31:08 +0700

    closure fix

--- pages/closure.js
+++ pages/closure.js
@@ -1,28 +1,24 @@
-import React from "react"
-import ReactDom from "react-dom"
+import ReactDOM from "react"
 
-class Closure extends React.Component{
+const Closure = () => {
     const printAll = () => {
         const name = "Ivan"
         const surname = "Ivanov"
-            const printName = () => {
+        const printName = () => {
             console.log(name + surname)
+            return name + surname
         }
         return printName
     }
     const printFinal = printAll()
 
-
-    render(){
-        const h1 = ReactDOM.createElement('h1', null, 'Привет МИР!')
-        return(
-
-            <div className="someClass">
-                {h1}
-                {/*{printFinal}*/}
-            </div>
-        )
-    }
+    const h1 = ReactDOM.createElement('h1', null, 'Привет МИР!')
+    return(
+        <div className="someClass">
+            {h1}
+            {printFinal()}
+        </div>
+    )
 }
 
 export default Closure
